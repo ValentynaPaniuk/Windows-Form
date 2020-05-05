bb990e5845d524b2a96668676968f543e4ed74b2
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-05-05 11:56:11 +0300

    Telephone Directory

--- Classwork20200504_Telephone_Directory/Parent.Designer.cs
+++ Classwork20200504_Telephone_Directory/Parent.Designer.cs
@@ -28,27 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Lb_AllContacts = new System.Windows.Forms.ListBox();
             this.Bt_Add = new System.Windows.Forms.Button();
             this.Btn_Edit = new System.Windows.Forms.Button();
             this.Btn_Delete = new System.Windows.Forms.Button();
             this.Btn_Clear = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // Lb_AllContacts
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(187, 14);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(231, 277);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.Lb_AllContacts.FormattingEnabled = true;
+            this.Lb_AllContacts.Location = new System.Drawing.Point(150, 19);
+            this.Lb_AllContacts.Name = "Lb_AllContacts";
+            this.Lb_AllContacts.Size = new System.Drawing.Size(314, 277);
+            this.Lb_AllContacts.TabIndex = 0;
+            this.Lb_AllContacts.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Bt_Add
             // 
-            this.Bt_Add.Location = new System.Drawing.Point(29, 173);
+            this.Bt_Add.Location = new System.Drawing.Point(14, 178);
             this.Bt_Add.Name = "Bt_Add";
             this.Bt_Add.Size = new System.Drawing.Size(115, 23);
             this.Bt_Add.TabIndex = 1;
@@ -58,7 +61,7 @@
             // 
             // Btn_Edit
             // 
-            this.Btn_Edit.Location = new System.Drawing.Point(29, 202);
+            this.Btn_Edit.Location = new System.Drawing.Point(14, 207);
             this.Btn_Edit.Name = "Btn_Edit";
             this.Btn_Edit.Size = new System.Drawing.Size(115, 23);
             this.Btn_Edit.TabIndex = 1;
@@ -68,7 +71,7 @@
             // 
             // Btn_Delete
             // 
-            this.Btn_Delete.Location = new System.Drawing.Point(29, 231);
+            this.Btn_Delete.Location = new System.Drawing.Point(14, 236);
             this.Btn_Delete.Name = "Btn_Delete";
             this.Btn_Delete.Size = new System.Drawing.Size(115, 23);
             this.Btn_Delete.TabIndex = 1;
@@ -78,7 +81,7 @@
             // 
             // Btn_Clear
             // 
-            this.Btn_Clear.Location = new System.Drawing.Point(29, 260);
+            this.Btn_Clear.Location = new System.Drawing.Point(14, 265);
             this.Btn_Clear.Name = "Btn_Clear";
             this.Btn_Clear.Size = new System.Drawing.Size(115, 23);
             this.Btn_Clear.TabIndex = 1;
@@ -89,39 +92,53 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(39, 14);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 19);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(105, 142);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.Lb_AllContacts);
+            this.groupBox1.Controls.Add(this.Bt_Add);
+            this.groupBox1.Controls.Add(this.Btn_Clear);
+            this.groupBox1.Controls.Add(this.Btn_Edit);
+            this.groupBox1.Controls.Add(this.Btn_Delete);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 315);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "All contacts";
+            // 
             // Parent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 300);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.Btn_Clear);
-            this.Controls.Add(this.Btn_Delete);
-            this.Controls.Add(this.Btn_Edit);
-            this.Controls.Add(this.Bt_Add);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(510, 339);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Parent";
             this.Text = "All Contacts";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox Lb_AllContacts;
         private System.Windows.Forms.Button Bt_Add;
         private System.Windows.Forms.Button Btn_Edit;
         private System.Windows.Forms.Button Btn_Delete;
         private System.Windows.Forms.Button Btn_Clear;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Classwork20200504_Telephone_Directory/Parent.cs
+++ Classwork20200504_Telephone_Directory/Parent.cs
@@ -20,7 +20,7 @@ namespace Classwork20200504_Telephone_Directory
         //Cleare listbox
         private void Btn_Clear_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
+            Lb_AllContacts.Items.Clear();
             if (pictureBox1.Image != null)
             {
                 pictureBox1.Image.Dispose();
@@ -32,11 +32,11 @@ namespace Classwork20200504_Telephone_Directory
         private void Btn_Delete_Click(object sender, EventArgs e)
         {
             //Удаление выделенного элемента
-            if (listBox1.SelectedIndex == -1)//Если товар не выбран
+            if (Lb_AllContacts.SelectedIndex == -1)//Если товар не выбран
             {
                 MessageBox.Show("You have not selected a contact"); return;
             }
-            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            Lb_AllContacts.Items.RemoveAt(Lb_AllContacts.SelectedIndex);
             if (pictureBox1.Image != null)
             {
                 pictureBox1.Image.Dispose();
@@ -51,7 +51,7 @@ namespace Classwork20200504_Telephone_Directory
             Child addform = new Child(contact, true);
             if (addform.ShowDialog() == DialogResult.OK)
             {
-                listBox1.Items.Add(contact);
+                Lb_AllContacts.Items.Add(contact);
                 pictureBox1.Load(contact.Path);
             }
 
@@ -60,25 +60,27 @@ namespace Classwork20200504_Telephone_Directory
         private void Btn_Edit_Click(object sender, EventArgs e)
         {
             //редактирование контакта
-            if (listBox1.SelectedIndex == -1)//Если контакт не выбран
+            if (Lb_AllContacts.SelectedIndex == -1)//Если контакт не выбран
             {
                 MessageBox.Show("You have not selected a contact"); return;
             }
 
-            int n = listBox1.SelectedIndex;//запоминаем выделенный элемент
-            contact = (Contact)listBox1.Items[n];//Забираем ссылку на выделенный элемент
+            int n = Lb_AllContacts.SelectedIndex;//запоминаем выделенный элемент
+            contact = (Contact)Lb_AllContacts.Items[n];//Забираем ссылку на выделенный элемент
+
             Child editform = new Child(contact, false);
             editform.ShowDialog();
-            listBox1.Items.RemoveAt(n);//Удаляем выделенный элемент
-            listBox1.Items.Insert(n, contact);//и добавляем его снова в ту же позицию, чтобы он перерисовался в списке
-            listBox1.SelectedIndex = n; //Снова выделяем этот элемент
+            Lb_AllContacts.Items.RemoveAt(n);//Удаляем выделенный элемент
+            Lb_AllContacts.Items.Insert(n, contact);//и добавляем его снова в ту же позицию, чтобы он перерисовался в списке
+            Lb_AllContacts.SelectedIndex = n; //Снова выделяем этот элемент
+            
 
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int n = listBox1.SelectedIndex;//запоминаем выделенный элемент
-            contact = (Contact)listBox1.Items[n];//Забираем ссылку на выделенный элемент
+            int n = Lb_AllContacts.SelectedIndex;//запоминаем выделенный элемент
+            contact = (Contact)Lb_AllContacts.Items[n];//Забираем ссылку на выделенный элемент
             pictureBox1.Load(contact.Path);
         }
     }
